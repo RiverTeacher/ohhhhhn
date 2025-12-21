47955787cf0c3bea65ba63d3d1ed65279adc7715
RiverTeacher <75714738+RiverTeacher@users.noreply.github.com>
2025-12-22 01:51:14 +0900

    Update sw.js

--- koyama/sw.js
+++ koyama/sw.js
@@ -1,7 +1,26 @@
+// sw.js（オンライン専用）
+
 self.addEventListener('install', event => {
   self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
   self.clients.claim();
+});
+
+// キャッシュ一切しない
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return new Response(
+        'オンライン環境でご利用ください',
+        {
+          status: 503,
+          headers: {
+            'Content-Type': 'text/plain; charset=utf-8'
+          }
+        }
+      );
+    })
+  );
 });
